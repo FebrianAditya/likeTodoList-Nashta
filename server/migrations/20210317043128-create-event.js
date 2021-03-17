23492292903517e9d545d225cc26fbd77ced65bd
FebrianAditya <febrian.aditya@student.upi.edu>
2021-03-17 12:01:45 +0700

    initialize server-side, client-side, and add validation for table Event

---
+++ server/migrations/20210317043128-create-event.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      participant: {
+        type: Sequelize.ARRAY(Sequelize.TEXT)
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      note: {
+        type: Sequelize.TEXT
+      },
+      picture: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Events');
+  }
+};
